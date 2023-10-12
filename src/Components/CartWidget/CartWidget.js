4ba592a42b01b7156c4d5089033e2dbf825edeae
JackelineGuerreo <jacke1710@gmail.com>
2023-10-12 20:25:19 -0300

    primera entrega

---
+++ src/Components/CartWidget/CartWidget.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { IBsMinecart } from "react-icons/bs";
+const CartWidget = () => {
+    return (
+        <div>
+            <button>
+                <IBsMinecart/>
+            </button>
+        </div>
+    )
+}
+
+export default CartWidget
